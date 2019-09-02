5b4809650415999770657b49174cd628558a80e2
David H <iq.davidh@gmail.com>
2019-09-01 22:33:28 -0500

    ya con el bootstrap react

---
+++ src/app/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6">x</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
