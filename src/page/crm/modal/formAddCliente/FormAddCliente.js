91dbcaa5313bdb2b618b78f8eed858dbead928be
David H <iq.davidh@gmail.com>
2019-09-28 14:22:46 -0500

    ya tenemos el formulario de componentes para new cliente

--- src/page/crm/modal/formAddCliente/FormAddCliente.js
+++ src/page/crm/modal/formAddCliente/FormAddCliente.js
@@ -1,8 +1,69 @@
 import React, { Component } from 'react';
 import { Button, Modal } from 'react-bootstrap';
+import ObserverNewCliente from './ObserverNewCliente';
+import BuilderConfigControlItem from '../../../../components/edicion/BuilderConfigControlItem';
+import FactoryListaDataEdit from '../../../../components/edicion/FactoryListaDataEdit';
 
 class FormAddCliente extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.observerData = ObserverNewCliente;
+
+    let lista = [];
+
+    const builderNombre = new BuilderConfigControlItem('nombre', 'Nombre');
+    builderNombre.setIsRequired();
+    lista.push(builderNombre.getConfigControlItem());
+
+    /*------------------------------------------------------------*/
+    const builderAPaterno = new BuilderConfigControlItem(
+      'apaterno',
+      'A. Paterno'
+    );
+    builderAPaterno.setIsRequired();
+    lista.push(builderAPaterno.getConfigControlItem());
+
+    /*------------------------------------------------------------*/
+    const builderAMaterno = new BuilderConfigControlItem(
+      'amaterno',
+      'A. Materno'
+    );
+    builderAMaterno.setIsRequired();
+    lista.push(builderAMaterno.getConfigControlItem());
+
+    /*------------------------------------------------------------*/
+    {
+      const b = new BuilderConfigControlItem('email1', 'Email');
+      lista.push(b.getConfigControlItem());
+    }
+    /*------------------------------------------------------------*/
+    {
+      const b = new BuilderConfigControlItem('tel', 'Teléfono');
+      lista.push(b.getConfigControlItem());
+    }
+
+    this.listaConfigControl = lista;
+
+    this.state = {
+      clienteNew: {
+        nombre: '',
+        apaterno: '',
+        amaterno: '',
+        email1: '',
+        tel: ''
+      }
+    };
+  }
+
   render() {
+    const c = this.state.clienteNew;
+
+    let lista = FactoryListaDataEdit(
+      c,
+      this.listaConfigControl,
+      this.observerData
+    );
+
     return (
       <Modal show={this.props.isShow} onHide={event => this.props.onClose()}>
         <Modal.Header closeButton>
@@ -11,7 +72,11 @@ class FormAddCliente extends Component {
           </Modal.Title>
         </Modal.Header>
 
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Body>
+          <table className="table table-sm table-striped teditdata">
+            <tbody>{lista}</tbody>
+          </table>
+        </Modal.Body>
 
         <Modal.Footer>
           <Button variant="primary">
---
+++ src/page/crm/modal/formAddCliente/ObserverNewCliente.js
@@ -0,0 +1,5 @@
+import ObserverDataEdit from '../../../../lib/ObserverDataEdit';
+
+const ObserverNewCliente = new ObserverDataEdit();
+
+export default ObserverNewCliente;
