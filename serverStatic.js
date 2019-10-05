c746e756a4053fa53768290017c55e63bc660dbc
davidh <iq.davidh@gmail.com>
2019-10-04 22:45:43 -0500

    problema copn el router arregaldo

--- serverStatic.js
+++ serverStatic.js
@@ -4,7 +4,7 @@
  */
 
 const express = require('express');
-
+const path = require('path');
 const app = express();
 
 app.use(express.static('build'));
@@ -12,6 +12,15 @@ app.use(express.static('build'));
 app.get("/debug", function (req, res) {
 	res.send('Im static');
 });
+
+app.get('/*', function(req, res) {
+	res.sendFile(path.join(__dirname, 'build/index.html'), function(err) {
+		if (err) {
+			res.status(500).send(err)
+		}
+	})
+});
+
 
 const port = 4004;
 app.listen(port, () => console.log(`static server running on port ${port}!`));
